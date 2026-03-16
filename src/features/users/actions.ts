8eb4434451c3119dec0c8faf026b37fe803acb7f
aweston <austin.inhyuk.chong@gmail.com>
2026-03-16 11:16:16 -0700

    Implementing Caching

---
+++ src/features/users/actions.ts
@@ -0,0 +1,16 @@
+"use server"
+
+import { UserTable } from "@/drizzle/schema";
+import { getUserIdTag } from "./dbCache";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import { db } from "@/drizzle/db";
+import { eq } from "drizzle-orm";
+
+export async function getUser(id: string) {
+    "use cache";
+    cacheTag(getUserIdTag(id));
+
+    return db.query.UserTable.findFirst({
+        where: eq(UserTable.id, id),
+    });
+}
