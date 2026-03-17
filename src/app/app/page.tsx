cfa5b1c71488927a58f25d5b069573685254212a
aweston <austin.inhyuk.chong@gmail.com>
2026-03-17 13:07:14 -0700

    Start Filtering System w/ mock data

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,7 +1,31 @@
+import { getCurrentUser } from "@/services/clerk/lib/getCurrentUser";
+import { Loader2Icon } from "lucide-react";
+import { Suspense } from "react";
+
 export default function AppPage() {
-    return (
-        <div className="container flex flex-col items-center justify-center h-screen gap-4">
-            <h1 className="text-4xl">Hi!</h1>
+  return (
+    <Suspense
+      fallback={
+        <div className="h-screen-header flex items-center justify-center">
+          <Loader2Icon className="size-24 animate-spin" />
         </div>
-    )
+      }
+    >
+        <CreatorInfos />
+    </Suspense>
+  );
+}
+
+async function CreatorInfos(filters: Array<string> = []) {
+    const { userId, redirectToSignIn } = await getCurrentUser();
+    
+    if (userId == null) return redirectToSignIn;
+
+    const creatorInfos = await getCreatorInfos(userId, filters);
+}
+
+async function getCreatorInfos(userId: string, filters: Array<string> = []) {
+    // need to have mock data
+    // use cache
+    // have filtering
 }
