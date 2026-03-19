d36fbc563d51722b9e7b8bc3e6873b7bc4a94566
aweston <austin.inhyuk.chong@gmail.com>
2026-03-19 11:47:17 -0700

    Start Adding Cards

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -13,19 +13,36 @@ export default function AppPage() {
         </div>
       }
     >
-        <CreatorInfos />
+      <CreatorInfos />
     </Suspense>
   );
 }
 
 async function CreatorInfos({ filters = {} }: { filters?: CreatorFilters }) {
-    const { userId, redirectToSignIn } = await getCurrentUser();
-    
-    if (userId == null) return redirectToSignIn;
+  const { userId, redirectToSignIn } = await getCurrentUser();
+  if (userId == null) return redirectToSignIn();
 
-    // const creators = await getCreatorsByFilters(filters);
-    const creators = await getCreatorsByFilters({median_views_min: 1000});
+  // const creators = await getCreatorsByFilters(filters);
+  const creators = await getCreatorsByFilters({ median_views_min: 1000 });
 
-    // TODO: render the creators
-    return <div>{creators[7].display_name}</div>;
+  if (creators.length === 0) {
+    return <NoCreatorsFound />;
+  }
+  // TODO: render the creators
+  return <div>{creators[7].display_name}</div>;
+}
+
+function NoCreatorsFound() {
+  return (
+    <div className="container my-4 max-w-5xl">
+      <h1 className="text-3xl md:text-4xl lg:text-5xl mb-4">
+        Welcome to CreatorHub
+      </h1>
+      <p className="text-muted-foreground mb-8">
+        To get started, try adjusting the filters related to the creator/(s) you're
+        interested in. The most popular creators will be displayed first.
+      </p>
+      <Card />
+    </div>
+  );
 }
--- src/features/creators/cache.ts
+++ src/features/creators/cache.ts
@@ -1,17 +1,28 @@
 // "use server"; ??
 
-import { getIdTag } from "@/lib/dataCache";
+import { getIdTag, getUserTag } from "@/lib/dataCache";
 import { revalidateTag } from "next/cache";
 
-export function getCreatorInfoUserTag(userId: string) {
-    return getIdTag("creators", userId);
-}
-
+// creator data for all users
 export function getCreatorIdTag(creatorId: string) {
     return getIdTag("creators", creatorId);
 }
 
+// user-specific tag for creator info
+export function getCreatorInfoUserTag(userId: string) {
+    return getUserTag("creators", userId);
+}
+
+// invalidate a specific creator + a user's creator data
 export function revalidateCreatorCache({ userId, creatorId }: { userId: string; creatorId: string }) {
     revalidateTag(getCreatorInfoUserTag(userId));
     revalidateTag(getCreatorIdTag(creatorId));
 }
+
+
+// export function getCreatorGlobalTag() {
+//     return getGlobalTag("creators");
+// }
+// export function revalidateAllCreatorsCache() {
+//     revalidateTag(getCreatorGlobalTag());
+// }
