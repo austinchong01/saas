76bc3b577e000a29f3441a5d008aac21ad7a15d9
Austin <austin.inhyuk.chong@gmail.com>
2026-04-06 14:19:47 -0700

    Server DB

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -2,26 +2,39 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-    server: {
-        DB_PASSWORD: z.string().min(1),
-        DB_HOST: z.string().min(1),
-        DB_PORT: z.string().min(1),
-        DB_NAME: z.string().min(1),
-        DB_USER: z.string().min(1),
-        ARCJET_KEY: z.string().min(1),
-        CLERK_SECRET_KEY: z.string().min(1),
-    },
+  server: {
+    DATABASE_URL: z.string().optional(),
+    DB_PASSWORD: z.string().optional(),
+    DB_HOST: z.string().optional(),
+    DB_PORT: z.string().optional(),
+    DB_NAME: z.string().optional(),
+    DB_USER: z.string().optional(),
+    ARCJET_KEY: z.string().min(1),
+    CLERK_SECRET_KEY: z.string().min(1),
+  },
 
-    // create a new env variable - DATABASE_URL
-    createFinalSchema: env => {
-        return z.object(env).transform(val => {
-            const { DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_USER, ...rest } = val;
-            return {
-                ...rest,
-                DATABASE_URL: `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
-            }
-        })
-    },
-    emptyStringAsUndefined: true,
-    experimental__runtimeEnv: process.env
+  createFinalSchema: (env) => {
+    return z.object(env).transform((val) => {
+      const {
+        DATABASE_URL,
+        DB_PASSWORD,
+        DB_HOST,
+        DB_PORT,
+        DB_NAME,
+        DB_USER,
+        ...rest
+      } = val;
+
+      const url =
+        DATABASE_URL ??
+        `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+      return {
+        ...rest,
+        DATABASE_URL: url,
+      };
+    });
+  },
+  emptyStringAsUndefined: true,
+  experimental__runtimeEnv: process.env,
 });
